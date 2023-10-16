bb19d48c4dffeff6804d26227cc2c91586f6628c
Aric Jiang <74705062+ninjarogue@users.noreply.github.com>
2023-10-16 15:47:31 +0800

    add community plugin recipes

--- plugins/community.lua
+++ plugins/community.lua
@@ -4,6 +4,17 @@ return {
   -- example of importing a plugin, comment out to use it or add your own
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
 
+  { import = "astrocommunity.pack.lua" },
+  { import = "astrocommunity.pack.typescript" },
+  { import = "astrocommunity.pack.html-css" },
+  { import = "astrocommunity.pack.json" },
+  { import = "astrocommunity.motion.leap-nvim" },
+  { import = "astrocommunity.motion.flit-nvim" },
+  { import = "astrocommunity.colorscheme.catppuccin" },
+  { import = "astrocommunity.motion.nvim-spider" },
+  { import = "astrocommunity.motion.nvim-surround" },
+  { import = "astrocommunity.editing-support.yanky-nvim" },
+  { import = "astrocommunity.git.diffview-nvim" },
   -- { import = "astrocommunity.colorscheme.catppuccin" },
   -- { import = "astrocommunity.completion.copilot-lua-cmp" },
 }
