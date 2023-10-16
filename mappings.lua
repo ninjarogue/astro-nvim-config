e02c73fcec4904d505a4907f74420edcbfe1a37b
Aric Jiang <74705062+ninjarogue@users.noreply.github.com>
2023-10-16 15:48:32 +0800

    add custom mappings

--- mappings.lua
+++ mappings.lua
@@ -9,14 +9,42 @@ return {
     -- second key is the lefthand side of the map
 
     -- navigate buffer tabs with `H` and `L`
-    -- L = {
-    --   function() require("astronvim.utils.buffer").nav(vim.v.count > 0 and vim.v.count or 1) end,
-    --   desc = "Next buffer",
-    -- },
-    -- H = {
-    --   function() require("astronvim.utils.buffer").nav(-(vim.v.count > 0 and vim.v.count or 1)) end,
-    --   desc = "Previous buffer",
-    -- },
+    L = {
+      function() require("astronvim.utils.buffer").nav(vim.v.count > 0 and vim.v.count or 1) end,
+      desc = "Next buffer",
+    },
+    H = {
+      function() require("astronvim.utils.buffer").nav(-(vim.v.count > 0 and vim.v.count or 1)) end,
+      desc = "Previous buffer",
+    },
+    U = { "<C-r>", desc = "Redo" },
+
+    ["<leader>gR"] = require("astronvim.utils").is_available "gitsigns.nvim" and {
+      function() require("gitsigns").reset_buffer() end,
+      desc = "Reset Git buffer",
+    } or nil,
+
+    ["<leader>gr"] = require("astronvim.utils").is_available "gitsigns.nvim" and {
+      function() require("gitsigns").reset_hunk() end,
+      desc = "Reset Git hunk",
+    } or nil,
+    ["<leader>gh"] = false,
+
+    ["<leader><leader>"] = {
+      function() require("telescope.builtin").find_files { hidden = true, no_ignore = true } end,
+      desc = "Find files",
+    },
+    ["<leader><cr>"] = { function() require("telescope.builtin").oldfiles() end, desc = "Find history" },
+    ["<leader>fR"] = { function() require("telescope.builtin").registers() end, desc = "Find registers" },
+    ["<leader>/"] = { function() require("telescope.builtin").live_grep() end, desc = "Find words" },
+    ["<leader>fg"] = {
+      function()
+        require("telescope.builtin").live_grep {
+          additional_args = function(args) return vim.list_extend(args, { "--hidden", "--no-ignore" }) end,
+        }
+      end,
+      desc = "Find words in all files",
+    },
 
     -- mappings seen under group name "Buffer"
     ["<leader>bD"] = {
@@ -32,6 +60,7 @@ return {
     ["<leader>b"] = { name = "Buffers" },
     -- quick save
     -- ["<C-s>"] = { ":w!<cr>", desc = "Save File" },  -- change description but the same command
+    ["<leader>fo"] = false,
   },
   t = {
     -- setting a mapping to false will disable it
