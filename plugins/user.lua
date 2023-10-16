87ad2d9df4550a14d38922f01d83039169d26525
Aric Jiang <74705062+ninjarogue@users.noreply.github.com>
2023-10-16 15:48:17 +0800

    add codeium and neogit

--- plugins/user.lua
+++ plugins/user.lua
@@ -9,4 +9,40 @@ return {
   --     require("lsp_signature").setup()
   --   end,
   -- },
+  {
+    "Exafunction/codeium.vim",
+    event = "User AstroFile",
+    config = function()
+      vim.keymap.set("i", "<C-y>", function() return vim.fn["codeium#Accept"]() end, { expr = true })
+      vim.keymap.set("i", "<c-;>", function() return vim.fn["codeium#CycleCompletions"](1) end, { expr = true })
+      vim.keymap.set("i", "<c-,>", function() return vim.fn["codeium#CycleCompletions"](-1) end, { expr = true })
+      vim.keymap.set("i", "<c-x>", function() return vim.fn["codeium#Clear"]() end, { expr = true })
+      vim.keymap.set("n", "<leader>;", function()
+        if vim.g.codeium_enabled == true then
+          vim.cmd "CodeiumDisable"
+        else
+          vim.cmd "CodeiumEnable"
+        end
+      end, { noremap = true, desc = "Toggle Codeium active" })
+    end,
+  },
+  {
+    "NeogitOrg/neogit",
+    event = "User AstroGitFile",
+    dependencies = {
+      "nvim-lua/plenary.nvim", -- required
+      "nvim-telescope/telescope.nvim", -- optional
+      "sindrets/diffview.nvim", -- optional
+      -- "ibhagwan/fzf-lua",              -- optional
+    },
+    config = true,
+    keys = function()
+      local prefix = "<leader>g"
+
+      return {
+        { prefix .. "nt", "<cmd>Neogit<CR>", desc = "Open Neogit Tab Page" },
+        { prefix .. "nc", "<cmd>Neogit commit<CR>", desc = "Open Neogit Commit Page" },
+      }
+    end,
+  },
 }
